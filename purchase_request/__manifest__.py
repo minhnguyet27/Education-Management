582ad44ae5e2d38790f576e735d2b7a7347c13a4
minhnguyet27 <mihnguyt2@gmail.com>
2024-12-06 09:54:04 +0700

    Add phân quyền

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -11,7 +11,9 @@
     'category': 'Purchases',
     'depends': ['base','hr','product','purchase'],
     'data': [ 
+        'security/group_security.xml',
         'security/ir.model.access.csv',
+        'security/ir.rule.xml',        
         'views/purchase_request.xml',
         'views/purchase_request_line.xml',
     ],
